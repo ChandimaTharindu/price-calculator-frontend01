441740f1d0e955659fe128398ee1125ce6f7ced9
ChandimaTharindu <44630103+ChandimaTharindu@users.noreply.github.com>
2021-02-22 08:27:12 +0530

    Update services.ts

--- src/app/services.ts
+++ src/app/services.ts
@@ -17,29 +17,4 @@ export class services {
     return this.httpClient.get<any>('http://localhost:8080/api_product/getAllPraduct');
   }
 
-
-  // getPrice(): Observable<any> {
-  //   return this.httpClient.get<any>('https://picsum.photos/v2/list?page=1&limit=5');
-  // }
-
-// addPost(uploadImageData): Observable<any> {
-//     return this.httpClient.post('http://localhost:8080/post/upload', uploadImageData, { observe: 'response' });
-// }
-// // getPost(imageName): Observable<any> {
-// //     return this.httpClient.get('http://localhost:8080/post/get/' + imageName)
-// // }
-// deletePost(id : string):Observable<any> {
-//   return this.httpClient.delete('http://localhost:8080/post/delete/'+ id);
-// }
-// getAllPost(): Observable<any> {
-//     return this.httpClient.get('http://localhost:8080/post/getall')
-// }
-
-//   updateLike(updateLike): Observable<any> {
-//     return this.httpClient.post(`http://localhost:8080/post/updatelike` ,updateLike, { observe: 'response' });
-// }
-
-//   updateDislike(updateDisLike): Observable<any> {
-//   return this.httpClient.post(`http://localhost:8080/post/updatedislike` ,updateDisLike, { observe: 'response' });
-// }
 }
